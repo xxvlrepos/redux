1d7da08f83bca1210dca305b67b4381cc6509418
xxvlrepos <ne_ya@me.com>
2022-04-05 21:15:29 +0300

    ui

---
+++ src/components/video_detail.js
@@ -0,0 +1,9 @@
+import react, {Component} from "react";
+
+class SearchBar extends Component{
+    render(){
+        return <input onChange={event => console.log(event.target.value)}/>
+    }
+}
+
+export default SearchBar;
